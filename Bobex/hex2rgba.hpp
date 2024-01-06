f64a55d125b27e600dfda25c0e41841e6778e138
Ririto Ninigaya <154670403+RiritoNinigaya@users.noreply.github.com>
2024-01-06 13:07:47 +0200

    Update hex2rgba.hpp

--- Bobex/hex2rgba.hpp
+++ Bobex/hex2rgba.hpp
@@ -2,7 +2,7 @@
 #include <string>
 #include "../Bobex/Includes.hpp"
 auto HexToRGBA = [](const std::string& Hex) {
-	int r, g, b, a;
+	int r, g, b;
 	sscanf_s(Hex.c_str(), "%02x%02x%02x", &r, &g, &b);
 	const ImVec4 color{ (static_cast<float>(r) / 255), (static_cast<float>(g) / 255), (static_cast<float>(b) / 255), 255 };
 	return color;
